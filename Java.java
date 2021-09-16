26f9edd57210f6bda9a0fbc75da4bd64c35b2239
Mateusz Mróz <71221358+MattyMroz@users.noreply.github.com>
2021-09-16 13:40:51 +0200

    Update Java.java

--- Java.java
+++ Java.java
@@ -6,3 +6,50 @@ public class Projekt1 {
     }
     
 }
+
+
+
+
+
+package projekt1;
+import java.util.Scanner;
+public class Projekt1 {
+
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Jak masz na imie?");
+        //scan.nextLine(); //Pobiera Enter i zapodziane znaki białe
+        //scan.nextInt();
+        //scan.nextDouble();
+        String imie = scan.nextLine();
+        System.out.println("Witaj " + imie);
+    }
+    
+}
+
+
+
+
+
+package projekt1;
+
+public class Projekt1 {
+
+    public static void main(String[] args) {
+
+        int x = 5;
+        double y = 5.5;
+        boolean z = true;
+
+        if (x < y) {
+            System.out.println("To jest " + x);
+            System.out.println("To jest " + x);
+        } else {
+            System.out.println("To jest " + y);
+        }
+
+    }
+}
+
+
