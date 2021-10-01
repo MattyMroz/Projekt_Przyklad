e4b4b2080f5a341ed722fd960a4f6771688f50c0
Mateusz Mróz <71221358+MattyMroz@users.noreply.github.com>
2021-10-01 10:07:54 +0200

    Update C++Szkoła.cpp

--- C++Szkoła.cpp
+++ C++Szkoła.cpp
@@ -581,3 +581,27 @@ void RomToDec(string x)
     }
     cout << display_dec;
 }
+
+#include <iostream>
+using namespace std;
+int a[10] = {2,4,5,1,-9,8,238,1,2,9};
+int main()
+{
+    for(int i=0; i<9;i++)
+    {
+        for(int j=0; j<9;j++)
+        {
+            if(a[j]>a[j+1])
+            {
+                swap(a[j],a[j+1]);
+            }
+        }
+
+    }
+    
+    for(int i=0;i<10;i++)
+    {
+        cout << a[i] << endl;
+    }
+    return 0;
+}
