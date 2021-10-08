c894022e82dd45a31df693d5e789388d8757cd81
Mateusz Mróz <71221358+MattyMroz@users.noreply.github.com>
2021-10-08 10:35:55 +0200

    Update C++Szkoła.cpp

--- C++Szkoła.cpp
+++ C++Szkoła.cpp
@@ -696,19 +696,21 @@ using namespace std;
 
 void sortowanie_przez_wstawianie(int n, int *tab)
 {
-    int pom, j;
-    for(int i=1; i<n; i++)
-    {
-        pom = tab[i];
-        j = i-1;
-        
-        while(j>=0 && tab[j]>pom) 
-        {
-            tab[j+1] = tab[j];
-            --j;
-        }
-        tab[j+1] = pom;
-    }     
+     int pom, j;
+     for(int i=1; i<n; i++)
+     {
+             //wstawienie elementu w odpowiednie miejsce
+             pom = tab[i]; //ten element będzie wstawiony w odpowiednie miejsce
+             j = i-1;
+             
+             //przesuwanie elementów większych od pom
+             while(j>=0 && tab[j]>pom) 
+             {
+                        tab[j+1] = tab[j]; //przesuwanie elementów
+                        --j;
+             }
+             tab[j+1] = pom; //wstawienie pom w odpowiednie miejsce
+     }     
 }
 
 int main()
@@ -738,5 +740,58 @@ int main()
     cin.ignore();
     cin.get();
     return 0;    
+} 
+
+// Sortowanie przez wstawianie według długości string
+#include <iostream>
+using namespace std;
+
+void sortowanie_przez_wstawianie(int n, string *tab)
+{
+    int j;
+    string pom;
+    for (int i = 1; i < n; i++)
+    {
+        pom = tab[i];
+        j = i - 1;
+
+        while (j >= 0 && tab[j].size() > pom.size())
+        {
+            tab[j + 1] = tab[j];
+            --j;
+        }
+        tab[j + 1] = pom;
+    }
+}
+
+int main()
+{
+    int n;
+    string *tab;
+    cout << "Podaj wielkość zbioru: ";
+    cin >> n;
+
+    tab = new string[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Podaj " << i + 1 << " element: ";
+        cin >> tab[i];
+    }
+
+    cout << "Elementy przed sortowaniem:\n";
+    for (int i = 0; i < n; i++)
+        cout << tab[i] << " ";
+
+    sortowanie_przez_wstawianie(n, tab);
+
+    cout << "\nElementy posortowaniem:\n";
+    for (int i = 0; i < n; i++)
+        cout << tab[i] << " ";
+
+    cin.ignore();
+    cin.get();
+    return 0;
+}
 
 
