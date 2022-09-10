1053a813d90fc52e05019b6e86246365347e30fd
Mateusz Mróz <71221358+MattyMroz@users.noreply.github.com>
2022-09-10 19:37:21 +0200

    Update python.py

--- python.py
+++ python.py
@@ -33,3 +33,59 @@ for line in a:
         continue
     else:
         b.write(line)
+
+        
+        
+        
+        
+# Kopiowanie i przetłumacznie (bardziej powolne) stron internetowych z użyciem Selenium i Googletrans
+
+# pip install -U selenium
+# pip install googletrans
+# pip install googletrans==3.1.0a0
+# ChromeDriver -> C:\ Program Files (x86)\chromedriver.exe
+
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from googletrans import Translator
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+
+# Lista klas do przekopiowania
+classes = ["tit", "chapter", "txt"]
+
+# Lista rozdziałów
+fristChapter = 1
+lastChapter = 10
+
+for i in range(fristChapter, lastChapter + 1):
+    driver.get(
+        "https://freewebnovel.com/i-was-a-sword-when-i-reincarnated/chapter-" + str(i) + ".html")
+
+    for i in classes:
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, i))
+        )
+        with open("1.txt", "a", encoding="utf-8") as f:
+            f.write(element.text)
+            f.write("\n")
+        f.close()
+
+driver.quit()
+
+# # Przedmacz każdą linijkę tekstu odzielnie
+# with open("1.txt", "r", encoding="utf-8") as f:
+#     lines = f.readlines()
+#     f.close()
+
+# with open("2.txt", "w", encoding="utf-8") as f:
+#     for line in lines:
+#         translator = Translator()
+#         translation = translator.translate(line, dest="pl")
+#         f.write(translation.text)
+#         f.write("\n")
+#     f.close()
