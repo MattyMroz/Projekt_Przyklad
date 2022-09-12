b33b5af9bf9901d037ac3999d3203d9cf733c515
Mateusz Mróz <71221358+MattyMroz@users.noreply.github.com>
2022-09-12 11:30:22 +0200

    Update Java_App.java

--- Java_App.java
+++ Java_App.java
@@ -17,3 +17,51 @@ Double a = Double.parseDouble(jTextField1.getText());
 Double b = Double.parseDouble(jTextField5.getText());
 Double c = a + b;
 jTextField6.setText(c.toString());
+
+
+
+
+// Zad 1
+
+public class Sum {
+    private double A, B, SUM;
+
+    public Sum() {}
+
+    public void setA(double A) {
+        this.A = A;
+    }
+
+    public double getA() {
+        return A;
+    }
+
+    public void setB(double B) {
+        this.B = B;
+    }
+
+    public double getB() {
+        return B;
+    }
+
+    public void setSUM() {
+        SUM = A + B;
+    }
+
+    public double getSUM() {
+        return SUM;
+    }
+}
+
+// Button CLICK
+Sum s = new Sum();
+Double A = Double.parseDouble(jTextField1.getText());
+Double B = Double.parseDouble(jTextField5.getText());
+
+s.setA(A);
+s.setB(B);
+s.setSUM();
+
+jTextField6.setText("" + s.getSUM());
+
+
