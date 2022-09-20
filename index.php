6ff2d4c24232c2f27279a17538d91d4c1fd1b030
Mateusz Mróz <71221358+MattyMroz@users.noreply.github.com>
2022-09-20 09:05:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -693,3 +693,22 @@ phpinfo() - informacjei o php
 </body>
 
 </html>
+
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+  Name: <input type="text" name="fname">
+  <input type="submit">
+</form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // collect value of input field
+    $name = htmlspecialchars($_REQUEST['fname']);
+    if (empty($name)) {
+        echo "Name is empty";
+    } else {
+        echo $name;
+    }
+}
+?>
+
