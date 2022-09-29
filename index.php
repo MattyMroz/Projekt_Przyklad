18f4b5151a7ac9e898a0a1e28defeefaa7c692a8
Mateusz Mróz <71221358+MattyMroz@users.noreply.github.com>
2022-09-29 10:17:26 +0200

    Update index.php

--- index.php
+++ index.php
@@ -867,5 +867,65 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 http://www.capaciouscore.pl/kursy/kurs-php/superglobalne/
 
 
+index.html
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="index.php" method="post">
+
+        <label for="A">Liczba A</label>
+        <br>
+        <input type="text" name="A" id="A">
+        <br>
+        <label for="B">Liczba B</label>
+        <br>
+        <input type="text" name="B" id="B">
+        <br>
+        <input type="submit" value="Wyślij">
+    </form>
+</body>
+
+</html>
+
+index.php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $C = $_GET["A"] + $_GET["B"];
+    echo $C;
+    ?>
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
